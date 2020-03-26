d76e29fa94a482fb1fb092686922dea0a50114df
karpuwapower1 <34105551+karpuwapower1@users.noreply.github.com>
2020-03-26 21:51:37 +0300

    Add find flight command

--- src/main/java/by/training/karpilovich/lowcost/command/Page.java
+++ src/main/java/by/training/karpilovich/lowcost/command/Page.java
@@ -1,15 +1,15 @@
 package by.training.karpilovich.lowcost.command;
 
 public enum Page {
-	
-	DEFAULT("/index.jsp"), SIGN_IN("/jsp/signin.jsp"), SIGN_UP("/jsp/signup.jsp");
-	
+
+	DEFAULT("/index.jsp"), SIGN_IN("/jsp/signin.jsp"), SIGN_UP("/jsp/signup.jsp"), RESULT("//TODO");
+
 	private String address;
-	
+
 	private Page(String address) {
 		this.address = address;
 	}
-	
+
 	public String getAddress() {
 		return address;
 	}
