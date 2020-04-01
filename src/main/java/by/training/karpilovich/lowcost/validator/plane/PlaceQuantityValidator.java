46117688f9582e8797508773322fb9a32bcdf0ce
karpuwapower1 <34105551+karpuwapower1@users.noreply.github.com>
2020-04-01 23:02:31 +0300

    Add validators for plane

---
+++ src/main/java/by/training/karpilovich/lowcost/validator/plane/PlaceQuantityValidator.java
@@ -0,0 +1,23 @@
+package by.training.karpilovich.lowcost.validator.plane;
+
+import by.training.karpilovich.lowcost.exception.ValidatorException;
+import by.training.karpilovich.lowcost.util.MessageType;
+import by.training.karpilovich.lowcost.validator.Validator;
+
+public class PlaceQuantityValidator extends Validator {
+
+	private int quantity;
+
+	public PlaceQuantityValidator(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public void validate() throws ValidatorException {
+		if (quantity <= 0) {
+			throw new ValidatorException(MessageType.INVALID_PLANE_PASSENGER_QUANTITY.getMessage());
+		}
+		continueValidate();
+	}
+
+}
