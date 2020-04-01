7fb27bc97777888b110e7d7049d88a1f638c4bef
karpuwapower1 <34105551+karpuwapower1@users.noreply.github.com>
2020-04-01 23:02:57 +0300

    Add validator for city

---
+++ src/main/java/by/training/karpilovich/lowcost/validator/city/CityNameValidator.java
@@ -0,0 +1,23 @@
+package by.training.karpilovich.lowcost.validator.city;
+
+import by.training.karpilovich.lowcost.exception.ValidatorException;
+import by.training.karpilovich.lowcost.util.MessageType;
+import by.training.karpilovich.lowcost.validator.Validator;
+
+public class CityNameValidator extends Validator {
+
+	private String name;
+
+	public CityNameValidator(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void validate() throws ValidatorException {
+		if (name == null || name.isEmpty()) {
+			throw new ValidatorException(MessageType.INVALID_CITY_NAME.getMessage());
+		}
+		continueValidate();
+	}
+
+}
