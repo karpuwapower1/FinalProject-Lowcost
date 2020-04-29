81d75da0df84bcf0c5da97a57511acd5ef5c6518
karpuwapower1 <34105551+karpuwapower1@users.noreply.github.com>
2020-04-29 16:22:59 +0300

    Add commands for delete and update flight

---
+++ src/main/java/by/training/karpilovich/lowcost/command/impl/DeleteFlightCommand.java
@@ -0,0 +1,28 @@
+package by.training.karpilovich.lowcost.command.impl;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import by.training.karpilovich.lowcost.command.Command;
+import by.training.karpilovich.lowcost.command.JSPParameter;
+import by.training.karpilovich.lowcost.command.Page;
+import by.training.karpilovich.lowcost.exception.ServiceException;
+
+public class DeleteFlightCommand implements Command {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String flightId = request.getParameter(JSPParameter.FLIGHT_ID);
+		try {
+			getFlightService().removeFlight(flightId);
+			return new ShowAllFlightsCommand().execute(request, response);
+		} catch (ServiceException e) {
+			setErrorMessage(request, response.getLocale(), e.getMessage());
+			return Page.SHOW_FLIGHTS.getAddress();
+		}
+	}
+}
