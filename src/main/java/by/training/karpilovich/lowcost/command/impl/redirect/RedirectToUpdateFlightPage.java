bccf4de1bfb9022fe0a47cc075fa51374551323b
karpuwapower1 <34105551+karpuwapower1@users.noreply.github.com>
2020-04-29 16:24:01 +0300

    Add command to redirect to update flight page

---
+++ src/main/java/by/training/karpilovich/lowcost/command/impl/redirect/RedirectToUpdateFlightPage.java
@@ -0,0 +1,31 @@
+package by.training.karpilovich.lowcost.command.impl.redirect;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import by.training.karpilovich.lowcost.command.Attribute;
+import by.training.karpilovich.lowcost.command.Command;
+import by.training.karpilovich.lowcost.command.JSPParameter;
+import by.training.karpilovich.lowcost.command.Page;
+import by.training.karpilovich.lowcost.entity.Flight;
+import by.training.karpilovich.lowcost.exception.ServiceException;
+
+public class RedirectToUpdateFlightPage implements Command {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String flightId = request.getParameter(JSPParameter.FLIGHT_ID);
+		try {
+			Flight flight = getFlightService().getFlightById(flightId);
+			request.setAttribute(Attribute.FLIGHT.toString(), flight);
+			return Page.UPDATE_FLIGHT.getAddress();
+		} catch (ServiceException e) {
+			setErrorMessage(request, response.getLocale(), e.getMessage());
+			return Page.SHOW_FLIGHTS.getAddress();
+		}
+	}
+}
